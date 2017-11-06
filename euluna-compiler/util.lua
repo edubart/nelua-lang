683bfdcc9a56ec0b1bb3d6bc92a0bba0032ace5d
Eduardo Bart <edub4rt@gmail.com>
2017-11-06 20:58:01 -0200

    Remove tohex

--- euluna-compiler/util.lua
+++ euluna-compiler/util.lua
@@ -1,11 +1,5 @@
 local util = {}
 
-function util.tohex(s)
-	return (string.gsub(s, ".", function (c)
-		return string.format("%.2x", string.byte(c))
-	end))
-end
-
 function util.sha1sum(s)
   return require('lsha2').hash256(s)
 end
