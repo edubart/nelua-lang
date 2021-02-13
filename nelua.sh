c7cb27624a9f2fbe871b81e8f02206712f163c59
Eduardo Bart <edub4rt@gmail.com>
2021-02-13 12:58:10 -0300

    Add fallback to `realpath` in `nelua.sh` (for Mac OS X)

--- nelua.sh
+++ nelua.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# implement realpath case it's not present (Mac OS X for example)
+if ! [ -x "$(command -v realpath)" ]; then
+  realpath() {
+    OURPWD=$PWD
+    cd "$(dirname "$1")"
+    LINK=$(readlink "$(basename "$1")")
+    while [ "$LINK" ]; do
+      cd "$(dirname "$LINK")"
+      LINK=$(readlink "$(basename "$1")")
+    done
+    REALPATH="$PWD/$(basename "$1")"
+    cd "$OURPWD"
+    echo "$REALPATH"
+  }
+fi
+
 # detect the current directory for this script
 SCRIPT=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT)
